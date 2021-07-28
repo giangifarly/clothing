0301e9296c2af1aadfa11449ccf2047660c1f25d
giangifarly <>
2021-07-28 22:53:27 +0700

    fix redirect from page login and change password

--- application/views/profile.php
+++ application/views/profile.php
@@ -1,62 +1,54 @@
 <section class="u-align-left u-clearfix u-section-1" id="carousel_d1af">
-    <div class="u-clearfix u-sheet u-sheet-1">
-        <div class="u-align-center u-black u-container-style u-group u-radius-50 u-shape-round u-group-1">
-          <div class="u-container-layout u-valign-middle-xs u-container-layout-1">
-            <h2 class="u-custom-font u-font-arial u-text u-text-1">Your Profile</h2>
-          </div>
-        </div>
-        <div class="u-image u-image-circle u-image-1" alt="" data-image-width="1280" data-image-height="1280"></div>
-        <div class="u-clearfix u-gutter-0 u-layout-wrap u-layout-wrap-1">
-          <div class="u-layout">
-            <div class="u-layout-row">
-              <div class="u-container-style u-layout-cell u-size-12 u-layout-cell-1">
-                <div class="u-container-layout u-container-layout-2">
-                  <h2 class="u-custom-font u-font-oswald u-text u-text-2">&nbsp; &nbsp;Nama&nbsp; :</h2>
-                  <h2 class="u-custom-font u-font-oswald u-text u-text-3">&nbsp; &nbsp;E-mail :</h2>
-                </div>
-              </div>
-              <div class="u-container-style u-layout-cell u-size-48 u-layout-cell-2">
-                <div class="u-container-layout u-valign-bottom-lg u-valign-bottom-md u-valign-bottom-sm u-valign-bottom-xl u-container-layout-3">
-                  <h2 class="u-custom-font u-font-oswald u-text u-text-4">Nama Member</h2>
-                  <h2 class="u-custom-font u-font-oswald u-text u-text-5">Emailmember@gmail.com</h2>
-                  <a href="<?php echo site_url('pages/edit_profile') ?>" data-page-id="533293" class="u-border-2 u-border-black u-btn u-button-style u-hover-black u-none u-text-black u-text-hover-white u-btn-1">edit profile</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="u-align-center u-black u-clearfix u-section-2" id="sec-07cd">
-      <div class="u-clearfix u-sheet u-sheet-1">
-        <h3 class="u-custom-font u-font-arial u-text u-text-default u-text-1">Ubah Password</h3>
-        <div class="u-form u-form-1">
-          <form action="#" method="POST" class="u-clearfix u-form-spacing-16 u-form-vertical u-inner-form" style="padding: 41px;" source="email">
-            <input type="hidden" id="siteId" name="siteId" value="531722">
-            <input type="hidden" id="pageId" name="pageId" value="533249">
-            <div class="u-form-group u-form-group-1">
-              <label for="text-89c5" class="u-label">Password Lama</label>
-              <input type="text" placeholder="Masukan password lama" id="text-89c5" name="text" class="u-border-1 u-border-white u-input u-input-rectangle" required="required">
-            </div>
-            <div class="u-form-group u-form-group-2">
-              <label for="text-eb5b" class="u-label">Password Baru</label>
-              <input type="text" placeholder="Masukan password baru" id="text-eb5b" name="text-1" class="u-border-1 u-border-white u-input u-input-rectangle" required="required">
-            </div>
-            <div class="u-form-group u-form-group-3">
-              <label for="text-b7ad" class="u-label">Ulangi Password Baru</label>
-              <input type="text" placeholder="Masukan password baru" id="text-b7ad" name="text-2" class="u-border-1 u-border-white u-input u-input-rectangle" required="required">
-            </div>
-            <div class="u-align-center u-form-group u-form-submit u-form-group-4">
-              <a href="#" class="u-border-2 u-border-active-black u-border-hover-white u-border-white u-btn u-btn-submit u-button-style u-hover-white u-none u-text-hover-black u-text-white u-btn-1">Ubah Password<br>
-              </a>
-              <input type="submit" value="submit" class="u-form-control-hidden">
-            </div>
-            <div class="u-form-send-message u-form-send-success">Thank you! Your message has been sent.</div>
-            <div class="u-form-send-error u-form-send-message">Unable to send your message. Please fix errors then try again.</div>
-            <input type="hidden" value="" name="recaptchaResponse">
-          </form>
-        </div>
-      </div>
-    </section>
-  </body>
+	<div class="u-clearfix u-sheet u-sheet-1">
+		<div class="u-align-center u-black u-container-style u-group u-radius-50 u-shape-round u-group-1">
+			<div class="u-container-layout u-valign-middle-xs u-container-layout-1">
+				<h2 class="u-custom-font u-font-arial u-text u-text-1">Your Profile</h2>
+			</div>
+		</div>
+		<div class="u-image u-image-circle u-image-1" alt="" data-image-width="1280" data-image-height="1280"></div>
+		<div class="u-clearfix u-gutter-0 u-layout-wrap u-layout-wrap-1">
+			<div class="u-layout">
+				<div class="u-layout-row">
+					<div class="u-container-style u-layout-cell u-size-12 u-layout-cell-1">
+						<div class="u-container-layout u-container-layout-2">
+							<h2 class="u-custom-font u-font-oswald u-text u-text-2">&nbsp; &nbsp;Nama&nbsp; :</h2>
+							<h2 class="u-custom-font u-font-oswald u-text u-text-3">&nbsp; &nbsp;E-mail :</h2>
+						</div>
+					</div>
+					<div class="u-container-style u-layout-cell u-size-48 u-layout-cell-2">
+						<div class="u-container-layout u-valign-bottom-lg u-valign-bottom-md u-valign-bottom-sm u-valign-bottom-xl u-container-layout-3">
+							<h2 class="u-custom-font u-font-oswald u-text u-text-4"><?= $this->session->userdata('nama_lengkap') ?></h2>
+							<h2 class="u-custom-font u-font-oswald u-text u-text-5"><?= $this->session->userdata('email') ?></h2>
+							<a href="<?php echo site_url('pages/edit_profile') ?>" data-page-id="533293" class="u-border-2 u-border-black u-btn u-button-style u-hover-black u-none u-text-black u-text-hover-white u-btn-1">edit profile</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="u-align-center u-black u-clearfix u-section-2" id="sec-07cd">
+	<div class="u-clearfix u-sheet u-sheet-1">
+		<h3 class="u-custom-font u-font-arial u-text u-text-default u-text-1">Ubah Password</h3>
+		<div class="u-form u-form-1">
+			<form action="<?= site_url('user_control/updatePassword') ?>" method="post" style="padding: 41px;">
+				<div class="u-form-group">
+					<label for="text-89c5" class="u-label">Password Lama</label>
+					<input type="password" placeholder="Masukan password lama" name="old_password" class="u-border-1 u-border-white u-input u-input-rectangle" required="required">
+				</div>
+				<div class="u-form-group">
+					<label for="text-eb5b" class="u-label">Password Baru</label>
+					<input type="password" placeholder="Masukan password baru" name="new_password" class="u-border-1 u-border-white u-input u-input-rectangle" required="required">
+				</div>
+				<div class="u-form-group">
+					<label for="text-b7ad" class="u-label">Ulangi Password Baru</label>
+					<input type="password" placeholder="Masukan password baru" name="retype_new_password" class="u-border-1 u-border-white u-input u-input-rectangle" required="required">
+				</div>
+				<input type="submit" value="Ganti Password" class="u-border-2 u-border-active-black u-border-hover-white u-border-white u-btn u-btn-submit u-button-style u-hover-white u-none u-text-hover-black u-text-white u-btn-1">
+			</form>
+		</div>
+	</div>
+</section>
+</body>
+
 </html>
