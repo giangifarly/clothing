38e3707a0189fc50e75889dee1e4f86e165b8853
giangifarly <>
2021-07-31 12:15:11 +0700

    Update _bottom_template.php

--- application/views/admin/_partials/_bottom_template.php
+++ application/views/admin/_partials/_bottom_template.php
@@ -53,6 +53,7 @@
 		oFReader.readAsDataURL(document.getElementById("image-source").files[0]);
 
 		oFReader.onload = function(oFREvent) {
+			document.getElementById("image-preview").src = oFREvent.target.result;
 		};
 	};
 
